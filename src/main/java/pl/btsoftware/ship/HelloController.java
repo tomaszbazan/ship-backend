c1d1ede732cf941ed8b0d2272f0e95f3c1be2c2c
Tomasz Bazan <tomasz.bazan.89@gmail.com>
2021-03-02 00:11:37 +0100

    pay technical debt

--- src/main/java/pl/btsoftware/ship/HelloController.java
+++ src/main/java/pl/btsoftware/ship/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
-    String home() {
+    public String home() {
         return "Hello";
     }
 }
